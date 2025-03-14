00586aeb34b28431fb2ec544faf69b5414d2fb93
is61b <is61blp3i@gmail.com>
2025-03-14 09:46:30 +0700

    layout pt3 (menu)

--- resources/views/layout/dosen.blade.php
+++ resources/views/layout/dosen.blade.php
@@ -17,13 +17,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Mahasiswa</a>
+                    <a class="nav-link" aria-current="page" href="/mahasiswa">Mahasiswa</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Dosen</a>
+                    <a class="nav-link active" href="/dosen">Dosen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Galeri</a>
+                    <a class="nav-link" href="/galeri">Galeri</a>
                 </li>
 
                 </ul>
