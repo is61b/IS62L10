096745a280ed03b41b8f48133492423816ae7152
is61b <is61blp3i@gmail.com>
2025-03-14 10:08:02 +0700

    layout pt4 (konten)

--- resources/views/layout/dosen.blade.php
+++ resources/views/layout/dosen.blade.php
@@ -35,7 +35,22 @@
         </div>
     </nav>
 
-    <div>
+    <div class="container text-center mt-3 bg-white">
+        <h2 class="mb-3">Data Dosen</h2>
+        <div class="row">
+            <div class="m-auto col-6">
+                <ol class="list-group">
+                    @forelse ($data_dos as $dosen)
+                        <li class="list-group-item">{{$dosen}}</li>
+                    @empty
+                    <div class="alert alert-secondary" role="alert">
+                        Maaf, Data Dosen Tidak Tersedia
+                    </div>
+                    @endforelse
+                </ol>
+            </div>
+        </div>
+
         {{-- konten --}}
     </div>
 
