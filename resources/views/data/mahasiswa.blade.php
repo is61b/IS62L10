0734da3fb62bf07a960a21215a19920c9edd621c
is61b <is61blp3i@gmail.com>
2025-03-14 10:56:55 +0700

    backup (layout include)

---
+++ resources/views/data/mahasiswa.blade.php
@@ -0,0 +1,23 @@
+@include('layout.header',['title' => 'Data Mahasiswa'])
+
+    <div class="container text-center mt-3 bg-white">
+        <h2 class="mb-3">Data mahasiswa</h2>
+        <div class="row">
+            <div class="m-auto col-6">
+                <ol class="list-group">
+                    @forelse ($data_mhs as $mhs)
+                        <li class="list-group-item">{{$mhs}}</li>
+                    @empty
+                    <div class="alert alert-secondary" role="alert">
+                        Maaf, Data Mahasiswa Tidak Tersedia
+                    </div>
+                    @endforelse
+                </ol>
+            </div>
+        </div>
+
+        {{-- konten --}}
+    </div>
+
+
+@include('layout.footer')
