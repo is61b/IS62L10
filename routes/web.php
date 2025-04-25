ac3d6c4adce4e84865a385eb32acc37d442b92be
is61b <is61blp3i@gmail.com>
2025-04-25 09:15:59 +0700

    commit asset lainnya

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
 
@@ -50,7 +50,7 @@ Route::get('/', function () {
 //     return view('mahasiswa',compact('nama','nilai','nilai2'));
 // });
 
-Route::get('/master', function () {
+Route::get('/', function () {
     $data_mhs = ["Abdul","Adha","Aidil","Alif","Asfal"];
     return view('layout.master',compact('data_mhs'));
 });
