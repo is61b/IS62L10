5b23e4c1c7c9aba6d17643d82dc0b486547cd006
is61b <is61blp3i@gmail.com>
2025-05-16 11:33:37 +0700

    store dosen

--- resources/views/Dosen/form.blade.php
+++ resources/views/Dosen/form.blade.php
@@ -8,18 +8,19 @@
                 <div class="card-header">Form Tambah Data</div>
 
                 <div class="card-body">
-                    <form>
+                    <form method="post" action="/dosen" enctype="multipart/form-data">
+                        @csrf
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">NIDN</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="text" name="nidn" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Nama Lengkap</label>
-                            <input type="text" class="form-control" id="exampleInputPassword1">
+                            <input type="text" name="nama" class="form-control" id="exampleInputPassword1">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="exampleInputPassword1">
+                            <input type="email" name="email" class="form-control" id="exampleInputPassword1">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Rumpun</label>
@@ -36,7 +37,7 @@
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">No Handphone</label>
-                            <input type="nohp" class="form-control" id="exampleInputPassword1">
+                            <input type="nohp" name="nohp" class="form-control" id="exampleInputPassword1">
                         </div>
 
                         <button type="submit" class="btn btn-primary">Tambah</button>
--- routes/web.php
+++ routes/web.php
@@ -78,3 +78,4 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 //Data Dosen
 Route::get('/dosen', [dosenController::class, 'index']);
 Route::get('/dosen/tambah', [dosenController::class, 'create']);
+Route::post('/dosen', [dosenController::class, 'store']);
