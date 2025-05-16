249d3e4a1e6b2a2b9ae5bb62dfed4dd9efa3a444
is61b <is61blp3i@gmail.com>
2025-05-16 10:59:05 +0700

    form tambah dosen

--- app/Http/Controllers/dosenController.php
+++ app/Http/Controllers/dosenController.php
@@ -23,6 +23,7 @@ class dosenController extends Controller
     public function create()
     {
         // menampilkan form tambah
+        return view('Dosen.form');
     }
 
     /**
--- resources/views/Dosen/index.blade.php
+++ resources/views/Dosen/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><a href="" class="btn btn-primary btn-sm">Tambah Data</a></div>
+                <div class="card-header"><a href="/dosen/tambah" class="btn btn-primary btn-sm">Tambah Data</a></div>
 
                 <div class="card-body">
 
